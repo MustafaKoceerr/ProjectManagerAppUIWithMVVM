528a1bdc1bfda844883c0141aeca4bad5a8a7d8b
MustafaKoceerr <mustafakoceerr@hotmail.com>
2024-09-16 12:45:56 +0300

    Bound with team and archive adapter

--- app/src/main/java/com/android/example/projectmanagerappwithmvvm/activity/ProfileActivity.kt
+++ app/src/main/java/com/android/example/projectmanagerappwithmvvm/activity/ProfileActivity.kt
@@ -2,20 +2,53 @@ package com.android.example.projectmanagerappwithmvvm.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.example.projectmanagerappwithmvvm.R
+import com.android.example.projectmanagerappwithmvvm.adapter.ArchiveAdapter
+import com.android.example.projectmanagerappwithmvvm.adapter.MyTeamAdapter
+import com.android.example.projectmanagerappwithmvvm.databinding.ActivityProfileBinding
+import com.android.example.projectmanagerappwithmvvm.viewmodel.ProfileViewModel
 
 class ProfileActivity : AppCompatActivity() {
+    lateinit var binding: ActivityProfileBinding
+    val profileViewModel: ProfileViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        with(binding) {
+            val myTeamAdapter by lazy { MyTeamAdapter(profileViewModel.loadDataMyTeam()) }
+            recyclerTeam.apply {
+                adapter = myTeamAdapter
+                layoutManager = LinearLayoutManager(
+                    this@ProfileActivity,
+                    LinearLayoutManager.VERTICAL,
+                    false
+                )
+            }
+
+            val archiveAdapter by lazy { ArchiveAdapter(profileViewModel.loadDataArchive()) }
+            recyclerViewArchive.apply {
+                adapter = archiveAdapter
+                layoutManager = LinearLayoutManager(
+                    this@ProfileActivity,
+                    LinearLayoutManager.HORIZONTAL,
+                    false
+                )
+            }
+        }
+
+
     }
 }
