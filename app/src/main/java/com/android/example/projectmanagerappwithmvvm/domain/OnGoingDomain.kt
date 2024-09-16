ebac059587044830a059557c21d1324829fc9026
MustafaKoceerr <mustafakoceerr@hotmail.com>
2024-09-16 11:43:16 +0300

    ongoing Domain model added

---
+++ app/src/main/java/com/android/example/projectmanagerappwithmvvm/domain/OnGoingDomain.kt
@@ -0,0 +1,8 @@
+package com.android.example.projectmanagerappwithmvvm.domain
+
+data class OnGoingDomain(
+    var title:String,
+    var date:String,
+    var progressPercent:Int,
+    var picPath:String,
+)
